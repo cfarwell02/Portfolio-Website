dcd450bd568b0e4f79d2cc70ea654f8d3be262a2
cfarwell02 <farwell.connor@gmail.com>
2025-06-14 04:42:35 -0400

    Implemented a submit form to get emails directly from the site using FormSubmit.co

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
 
+// Tooltip activation
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+tooltipTriggerList.forEach((t) => new bootstrap.Tooltip(t));
 
+// AOS animation
+AOS.init();
+
+// GSAP animations
+gsap.from("#hero-name", {
+  y: -50,
+  opacity: 0,
+  duration: 1.75,
+  ease: "power2.out",
+});
+gsap.from(".lead", {
+  delay: 0.5,
+  y: 30,
+  opacity: 0,
+  duration: 1,
+  ease: "power2.out",
+});
